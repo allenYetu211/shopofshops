aeac725897a519be746817eeb3d55d855e001043
allenYetu211 <allenwill211@gmail.com>
2016-11-23 01:57:15 +0800

    修改

--- src/js/brand.js
+++ src/js/brand.js
@@ -1,26 +1,55 @@
-$(() => {
+    $(window).on('resize', function () {
+     waterfall.init();
+  })
+  $(window).on('load', function () {
+    waterfall.init();
+  })
+  $('.brands-items li').on('click', function() {
+    toggle.showToggle($(this));
+  })
   var waterfall = {
+    indexlist: '',
     init() {
-    let wf = $('#wf-parents').width();
-     if(wf >= 750){
-      this.positions(3)
-     }else if(wf <=750 && wf > 480){
-      this.positions(2)
-     }else if(wf <=480){
-      this.positions(1)
-     }
+      let poBox = $('.waterfall-items');
+      let pw = $('div[data-sw="1"]').innerWidth();
+
+      let dw = $('.waterfall-items').eq(0).innerWidth();
+      let countList = Math.floor(pw / dw);
+      let boxHeightList = [];
+      for(let i = 0; i < poBox.length; i++) {
+        let boxH = poBox.eq(i).innerHeight();
+        if (i < countList) {
+          poBox.eq(i).attr('style','');
+          boxHeightList[i] = boxH;
+        } else {
+          let minHeight = Math.min.apply(null, boxHeightList);
+          let minIndex = $.inArray(minHeight, boxHeightList)
+          poBox.eq(i).css({
+            'position':'absolute',
+            'top':minHeight,
+            'left':minIndex * dw
+          })
+           boxHeightList[minIndex] += poBox.eq(i).innerHeight();
+        }
+
+      }
     },
     positions(_count) {
       
     }
   }
-  $(window).on('resize', function () {
-     waterfall.init();
-  })
-  $(window).on('load', function() {
-    waterfall.init();
-    $('.waterfall-items').each(function () {
-           
+  var toggle = {
+    showToggle(_t) {
+      waterfall.init();
+      console.log($('.waterfall[data]'))
+      $('.waterfall').each( function () {
+        if($(this).data('sw') === _t.data('pois')){
+          $('.waterfall').removeClass('open');
+          $(this).addClass('open')
+        }
       })
-  })
-})
+    }
+  }
+
+
+
