0c2ad09f3cf802175544f065f398d488950c18d8
allenYetu211 <allenwill211@gmail.com>
2016-11-23 15:17:59 +0800

    调整细节

--- src/js/brand.js
+++ src/js/brand.js
@@ -1,55 +1,62 @@
-    $(window).on('resize', function () {
-     waterfall.init();
-  })
-  $(window).on('load', function () {
-    waterfall.init();
+  $(window).on('load', function() {
+    if($('.waterfall').length != 0) {
+      $('.waterfall').masonry({
+          itemSelector: '.waterfall-items',
+          columnWidth: 200
+      });
+      $('.waterfall').eq(0).nextAll().hide();
+    }
+  shopLogo.init()
   })
   $('.brands-items li').on('click', function() {
-    toggle.showToggle($(this));
-  })
-  var waterfall = {
-    indexlist: '',
-    init() {
-      let poBox = $('.waterfall-items');
-      let pw = $('div[data-sw="1"]').innerWidth();
-
-      let dw = $('.waterfall-items').eq(0).innerWidth();
-      let countList = Math.floor(pw / dw);
-      let boxHeightList = [];
-      for(let i = 0; i < poBox.length; i++) {
-        let boxH = poBox.eq(i).innerHeight();
-        if (i < countList) {
-          poBox.eq(i).attr('style','');
-          boxHeightList[i] = boxH;
-        } else {
-          let minHeight = Math.min.apply(null, boxHeightList);
-          let minIndex = $.inArray(minHeight, boxHeightList)
-          poBox.eq(i).css({
-            'position':'absolute',
-            'top':minHeight,
-            'left':minIndex * dw
-          })
-           boxHeightList[minIndex] += poBox.eq(i).innerHeight();
-        }
-
-      }
-    },
-    positions(_count) {
-      
+      toggle.showToggle($(this));
+  });
+  $('.js_shoplogo_perv').off().on('click',function() {
+    if(shopLogo.transformIndex <= 0){
+      return
     }
-  }
+    shopLogo.transformIndex--;
+    // console.log('less:',_count)
+    shopLogo.transformLogo(shopLogo.transformIndex);
+  })
+
+  $('.js_shoplogo_next').off().on('click',function() {
+    if(shopLogo.transformIndex >= $('.footer-shoplogo li').length - 6){
+      return
+    }
+    shopLogo.transformIndex++;
+    // console.log('add:',_count)
+    shopLogo.transformLogo(shopLogo.transformIndex);
+  })
+
   var toggle = {
-    showToggle(_t) {
-      waterfall.init();
-      console.log($('.waterfall[data]'))
-      $('.waterfall').each( function () {
-        if($(this).data('sw') === _t.data('pois')){
-          $('.waterfall').removeClass('open');
-          $(this).addClass('open')
-        }
+      showToggle(_t) {
+          // waterfall.init();
+          // console.log($('.waterfall[data]'))
+          $('.brands-items li').removeClass('active');
+          _t.addClass('active');
+          $('.waterfall').each(function() {
+              if ($(this).data('sw') === _t.data('pois')) {
+                  $('.waterfall').hide();
+                  $(this).show();
+
+              }
+          })
+      }
+  }
+  var shopLogo = {
+    transformIndex: 1,
+    init() {
+      let maxWidth = 0;
+      $('.footer-shoplogo li').each(function () {
+        maxWidth += $(this).innerWidth()
+      })
+      $('.footer-shoplogo ul').css('width', maxWidth);
+    },
+    transformLogo(_count) {
+      // console.log(_count)
+      $('.footer-shoplogo li').css({
+        'transform': 'translateX(-' + _count * 100 + '%)'
       })
     }
   }
-
-
-
