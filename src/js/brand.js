79434b1b74e03c364bb2549212919d3ad0fd00ba
allenYetu211 <allenwill211@gmail.com>
2016-11-30 01:00:30 +0800

    修改瀑布流

--- src/js/brand.js
+++ src/js/brand.js
@@ -1,47 +1,51 @@
   $(window).on('load', function() {
-    if($('.waterfall').length != 0) {
-      $('.waterfall').masonry({
-          itemSelector: '.waterfall-items',
-          columnWidth: 200
-      });
-      $('.waterfall').eq(0).nextAll().hide();
-    }
-  shopLogo.init()
+      if ($('.waterfall').length != 0) {
+        toggle.waterfall()
+        $('.waterfall').eq(0).nextAll().hide();
+      }
+      shopLogo.init()
   })
+
   $('.brands-items li').on('click', function() {
       toggle.showToggle($(this));
   });
-  $('.js_shoplogo_perv').off().on('click',function() {
-    if(shopLogo.transformIndex <= 0){
-      return
-    }
-    shopLogo.transformIndex--;
-    // console.log('less:',_count)
-    shopLogo.transformLogo(shopLogo.transformIndex);
+  $('.js_shoplogo_perv').off().on('click', function() {
+      if (shopLogo.transformIndex <= 0) {
+          return
+      }
+      shopLogo.transformIndex--;
+      // console.log('less:',_count)
+      shopLogo.transformLogo(shopLogo.transformIndex);
   })
 
-  $('.js_shoplogo_next').off().on('click',function() {
-    if(shopLogo.transformIndex >= $('.footer-shoplogo li').length - 6){
-      return
-    }
-    shopLogo.transformIndex++;
-    // console.log('add:',_count)
-    shopLogo.transformLogo(shopLogo.transformIndex);
+  $('.js_shoplogo_next').off().on('click', function() {
+      if (shopLogo.transformIndex >= $('.footer-shoplogo li').length - 6) {
+          return
+      }
+      shopLogo.transformIndex++;
+      // console.log('add:',_count)
+      shopLogo.transformLogo(shopLogo.transformIndex);
   })
 
-  $('.mobile-nav-more').on('click', function () {
-    toggle.mobilenav($(this));
+  $('.mobile-nav-more').on('click', function() {
+      toggle.mobilenav($(this));
   })
-  $('.language-mobile a').on('click', function () {
-    toggle.languageTooke($(this))
+  $('.language-mobile a').on('click', function() {
+      toggle.languageTooke($(this))
   })
   $('.mobile-informations i, .mobile-barcode').on('click', function() {
-    toggle.barcode();
+      toggle.barcode();
   })
   $('.barcode').on('click', function(el) {
-    el.stopPropagation();
+      el.stopPropagation();
   })
   var toggle = {
+      waterfall() {
+           $('.waterfall').masonry({
+              itemSelector: '.waterfall-items',
+              gutterWidth: 20
+          });
+      },
       showToggle(_t) {
           // waterfall.init();
           // console.log($('.waterfall[data]'))
@@ -54,33 +58,34 @@
 
               }
           })
+          this.waterfall();
       },
       barcode() {
-        $('.mobile-barcode').toggleClass('open');
+          $('.mobile-barcode').toggleClass('open');
       },
       languageTooke(_t) {
-         $('.language-mobile a').removeClass('active');
-         _t.addClass('active')
+          $('.language-mobile a').removeClass('active');
+          _t.addClass('active')
       },
-      mobilenav (_t) {
-        _t.toggleClass('open');
-        $('.mobile-nav-information').toggleClass('open');
-        $('body').toggleClass('open');
+      mobilenav(_t) {
+          _t.toggleClass('open');
+          $('.mobile-nav-information').toggleClass('open');
+          $('body').toggleClass('open');
       }
   }
   var shopLogo = {
-    transformIndex: 1,
-    init() {
-      let maxWidth = 0;
-      $('.footer-shoplogo li').each(function () {
-        maxWidth += $(this).innerWidth()
-      })
-      $('.footer-shoplogo ul').css('width', maxWidth);
-    },
-    transformLogo(_count) {
-      // console.log(_count)
-      $('.footer-shoplogo li').css({
-        'transform': 'translateX(-' + _count * 100 + '%)'
-      })
-    }
+      transformIndex: 1,
+      init() {
+          let maxWidth = 0;
+          $('.footer-shoplogo li').each(function() {
+              maxWidth += $(this).innerWidth()
+          })
+          $('.footer-shoplogo ul').css('width', maxWidth);
+      },
+      transformLogo(_count) {
+          // console.log(_count)
+          $('.footer-shoplogo li').css({
+              'transform': 'translateX(-' + _count * 100 + '%)'
+          })
+      }
   }
